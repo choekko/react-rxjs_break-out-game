f8d526a4ffa762916ee026cb1967a7d4cd85bb96
choekko <choekko@gmail.com>
2022-05-08 00:05:47 +0900

    fix: remove unnecessary code

--- src/features/physics/streams/ballStream.ts
+++ src/features/physics/streams/ballStream.ts
@@ -6,20 +6,18 @@ import { moveX, moveY } from 'features/physics/utils';
 import { Direction } from 'types/physics';
 import { gameStart$ } from 'features/display/StartButton';
 
-export const ballTimer$ = interval(Math.floor(1000 / BALL_SPEED)).pipe(skipUntil(gameStart$));
+export const ballInterval$ = interval(Math.floor(1000 / BALL_SPEED)).pipe(skipUntil(gameStart$));
 
 export const [ballDirectionX$, setBallDirectionX] = createSignal<Direction>();
 export const [ballDirectionY$, setBallDirectionY] = createSignal<Direction>();
 export const [hitTargetPosition$, setHitTargetPosition] = createSignal<[number, number]>();
 
-export const ballPositionX$ = ballTimer$.pipe(
-  skipUntil(gameStart$),
+export const ballPositionX$ = ballInterval$.pipe(
   withLatestFrom(ballDirectionX$.pipe(startWith<Direction>(1))),
   scan((currPosition, [, direction]) => moveX(currPosition, direction), BALL_START_POSITION_X),
 );
 
-export const ballPositionY$ = ballTimer$.pipe(
-  skipUntil(gameStart$),
+export const ballPositionY$ = ballInterval$.pipe(
   withLatestFrom(ballDirectionY$.pipe(startWith<Direction>(1))),
   scan((currPosition, [, direction]) => moveY(currPosition, direction), BALL_START_POSITION_Y),
 );
